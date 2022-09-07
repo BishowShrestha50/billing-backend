2ebd9de2edd0a14ebbdc6e4ef50a13016fb3598a
bishowshrestha50 <bishow.shrestha@berrybytes.com>
2022-09-07 18:24:27 +0545

    kubernet-deployment

--- internal/app/adapter/database.go
+++ internal/app/adapter/database.go
@@ -2,7 +2,6 @@ package adapter
 
 import (
 	"billing-backend/internal/app/models"
-	"fmt"
 	"os"
 
 	"github.com/jinzhu/gorm"
@@ -10,7 +9,8 @@ import (
 )
 
 func Initialize() (*gorm.DB, error) {
-	url := fmt.Sprintf("host=%v port=%s user=%v password=%v dbname=%s sslmode=disable", os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
+	//url := fmt.Sprintf("host=%v port=%s user=%v password=%v dbname=%s sslmode=disable", os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
+	url := os.Getenv("DB_SOURCE")
 	db, err := gorm.Open("postgres", url)
 	if err != nil {
 		return nil, err
